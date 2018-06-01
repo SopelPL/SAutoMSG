81c0e31c265fa6a77d1e86aa3728db536b09392f
SopelPL <34239613+SopelPL@users.noreply.github.com>
2018-06-01 17:52:05 +0200

    Add files via upload

---
+++ src/Listeners/InventoryClickListener.java
@@ -0,0 +1,74 @@
+package Listeners;
+
+import me.sopelplyt.sautomsg.Main;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class InventoryClickListener implements Listener{
+
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void onInventoryClick(InventoryClickEvent event) {
+		Player player = (Player) event.getWhoClicked(); 
+		ItemStack clicked = event.getCurrentItem(); 
+		Inventory inventory = event.getInventory(); 
+		if (inventory.getName().equals(Main.getInst().gui.getName())) {
+			if (clicked.getType() == Material.EMERALD_BLOCK) {
+				event.setCancelled(true);
+				player.closeInventory();
+				if(!Main.sendMsg){
+					Main.start();
+					Main.sendMsg = true;
+					Main.getInst().openGUI(player);
+					player.sendMessage(Main.getInst().colorMessage("&2&lWlaczyles &aautomatyczne wiadomosci!"));
+					return;
+				}else{
+					Main.getInst().openGUI(player);
+					player.sendMessage(Main.getInst().colorMessage("&cWiadomosci sa juz &a&lwlaczone&c!"));
+					return;
+				}
+			}else if (clicked.getType() == Material.REDSTONE_BLOCK){
+				event.setCancelled(true); 
+				player.closeInventory();
+				if(Main.sendMsg){
+					Main.stop();
+					Main.sendMsg = false;
+					Main.getInst().openGUI(player);
+					player.sendMessage(Main.getInst().colorMessage("&4&lWylaczyles &aautomatyczne wiadomosci!"));
+					return;
+				}else{
+					Main.getInst().openGUI(player);
+					player.sendMessage(Main.getInst().colorMessage("&cWiadomosci sa juz &4&lwylaczone&c!"));
+					return;
+				}
+			}else if (clicked.getType() == Material.GRASS || clicked.getType() == Material.DIRT){
+				event.setCancelled(true);
+				player.closeInventory();
+				Main.getInst().openGUI(player);
+				return;
+			}else if (clicked.getType() == Material.PAPER){
+				event.setCancelled(true);
+				player.closeInventory();
+				Main.getInst().openGUI(player);
+				return;
+			}else if (clicked.getType() == Material.TNT){
+				event.setCancelled(true);
+				player.closeInventory();
+				return;
+			}else if (clicked.getType() == Material.getMaterial(102)){
+				event.setCancelled(true);
+				player.closeInventory();
+				Main.getInst().openGUI(player);
+				return;
+			}else{
+				return;
+			}
+		}
+	}
+}
